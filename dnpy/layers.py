Fix softmax

dnpy/layers.py
@@ -153,11 +153,10 @@ class Softmax(Layer):
         self.output = exps/sums
 
     def backward(self):
+        self.parent.delta = np.zeros_like(self.output)
         m = self.output.shape[-1]
-        tmp = []
         for i in range(m):
             SM = self.output[:, i].reshape((-1, 1))
-            jaccobian = np.diagflat(self.parent.output[:, i]) - np.dot(SM, SM.T)
-            delta_i = np.dot(jaccobian, self.delta[:, i])
-            tmp.append(delta_i)
-        self.parent.delta = np.array(tmp).T
+            jac = np.diagflat(self.output[:, i]) - np.dot(SM, SM.T)
+            self.parent.delta[:, i] = np.dot(jac, self.delta[:, i])
+

examples/0_xor.py
@@ -15,10 +15,10 @@ def main():
                   [1, 0, 1],
                   [1, 1, 1]])
 
-    Y = np.array([[0],
+    Y = np.array([[-1],
                   [1],
                   [1],
-                  [0]])
+                  [-1]])
 
     x_train, y_train = X, Y
     x_test, y_test = X, Y
@@ -33,7 +33,7 @@ def main():
     l = Dense(l, 3)
     l = Relu(l)
     l = Dense(l, 1)
-    l = Sigmoid(l)
+    # l = Sigmoid(l)
     l_out = l
 
     # Build network