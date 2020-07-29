Fix mnist example + add smart derivatives + utils functions

dnpy/layers.py
@@ -184,6 +184,7 @@ class Softmax(Layer):
         self.oshape = self.parent.oshape
 
         self.stable = stable
+        self.ce_backward = False
 
     def forward(self):
         if self.stable:
@@ -196,12 +197,15 @@ class Softmax(Layer):
         self.output = exps/sums
 
     def backward(self):
-        self.parent.delta = np.zeros_like(self.output)
-        m = self.output.shape[0]
-        for i in range(m):
-            SM = self.output[i, :].reshape((-1, 1))
-            jac = np.diagflat(self.output[i, :]) - np.dot(SM, SM.T)
-            self.parent.delta[i, :] = np.dot(jac, self.delta[i, :])
+        if self.ce_backward:  # Only valid for a Cross-Entropy loss
+            self.parent.delta = self.output + self.delta
+        else:  # Generic
+            self.parent.delta = np.zeros_like(self.output)
+            m = self.output.shape[0]
+            for i in range(m):
+                SM = self.output[i, :].reshape((-1, 1))
+                jac = np.diagflat(self.output[i, :]) - np.dot(SM, SM.T)
+                self.parent.delta[i, :] = np.dot(jac, self.delta[i, :])
 
 
 class Dropout(Layer):

dnpy/utils.py
@@ -1,7 +1,41 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 
-def to_categorical(dataset, num_classes):
-    tmp = np.zeros((len(dataset), num_classes))
-    tmp[np.arange(len(dataset)), dataset] = 1.0
+def to_categorical(y, num_classes):
+    tmp = np.zeros((len(y), num_classes))
+    tmp[np.arange(len(y)), y] = 1.0
     return tmp
+
+
+def to_binary(y, label):
+    tmp = np.zeros_like(y)
+    tmp[y == label] = 1.0
+    return tmp
+
+
+def to_label(y):
+    return np.argmax(y, axis=1)
+
+
+def shuffle_dataset(x, y):
+    idxs = np.arange(len(x))
+    np.random.shuffle(idxs)
+    return x[idxs], y[idxs]
+
+
+def split_dataset(x, y, split=0.8):
+    size = int(len(x)*split)
+    return x[:size], y[:size]
+
+
+def show_dataset(x, y, size=1, show_rnd=True):
+    if show_rnd:
+        indices = np.random.randint(0, len(x), size=size)
+    else:
+        indices = np.arange(0, size)
+
+    for i, idx in enumerate(indices, 1):
+        print(f"[{i}/{size}] Index: {idx}\t Y: {y[idx]}")
+        plt.imshow(x[idx])
+        plt.show()

examples/2_iris.py
@@ -18,7 +18,7 @@ def main():
     Y = iris.target
 
     # Pre-processing
-    # Normalize
+    # Standarize
     X = (X - np.mean(X, axis=0))/np.std(X, axis=0)
 
     # Classes to categorical